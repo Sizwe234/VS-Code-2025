ce65a387eff524adc891dcc2380fd7611aba9bcd
Sizwe234 <161421362+Sizwe234@users.noreply.github.com>
2025-09-30 19:26:01 +0200

    Add files via upload

---
+++ GeometryHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Library
+{
+    public class GeometryHelper
+    {
+        public static double CircleArea(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Radius Cannot Be negative");
+            }
+            return Math.PI * radius * radius;
+        }
+
+        public static double RectangleArea(double length, double width)
+        {
+            if (length < 0 | width < 0) {
+                throw new ArgumentException("Dimensions cannot be Negative");
+            }
+            return length * width;
+        }
+    }
+}
---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿
+Imports Math_Library
+Module Module1
+
+
+    Sub Main()
+
+        Dim calc As New Calculator()
+
+        Console.WriteLine(calc.Add(10, 5))
+
+        Console.WriteLine(GeometryHelper.CircleArea(3.0))
+
+    End Sub
+
+End Module
